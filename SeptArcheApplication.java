1d888398bad41da83e706a7bc9330580346b76ed
Fatiocto <116719823+Fatiocto@users.noreply.github.com>
2023-04-19 23:52:47 +0200

    Add files via upload

---
+++ SeptArcheApplication.java
@@ -0,0 +1,13 @@
+package fr.librairie.septArche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptArcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeptArcheApplication.class, args);
+	}
+
+}
